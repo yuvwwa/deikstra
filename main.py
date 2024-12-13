d8ae4c17af37d46f1e723e98ae25ce1d1b7ecc56
yuvwwa <150217655+yuvwwa@users.noreply.github.com>
2024-12-13 14:57:34 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
 from deikstra import Graph
 
 graph = {
-    'A': {'B':6, 'C':10, 'F':1},
-    'B': {'C':8, 'D':5}, 
-    'C': {'D':9, 'F':11},
-    'D': {'E':4,},
-    'E': {'F':3},
-    'F': {'B':7}
+    'A': {'B':10, 'C':6, 'D':8},
+    'B': {'G':11, 'D':5}, 
+    'C': {'E':3},
+    'D': {'C':2, 'E':5, 'F':7, 'G':12},
+    'E': {'F':9, 'I':12},
+    'F': {'H':8, 'I':10},
+    'G': {'F':4, 'H':6},
+    'H': {'I':5},
+    'I': {'I':0}
 }
 
 graph0 = Graph(graph)
-result = graph0.deikstra('A')
+result, previous = graph0.deikstra('A')
+
+print(graph0.full_path(previous, 'A', 'I'))
+
 
 for vertex, distance in result.items():
     print(f"{vertex}: {distance}")
